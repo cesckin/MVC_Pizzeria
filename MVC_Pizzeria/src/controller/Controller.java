ceff9851eb2aa5796b86a2e788d6efe6b4d92d44
Cesco <Cesco@Winzoz11-Fisso>
2021-10-11 18:58:24 +0200

    Object update

--- MVC_Pizzeria/src/controller/Controller.java
+++ MVC_Pizzeria/src/controller/Controller.java
@@ -2,6 +2,10 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 import model.Ordinazione;
 import view.Grafica;
@@ -26,15 +30,32 @@ public class Controller implements ActionListener {
 			
 			Ordinazione ordinazione = new Ordinazione(0, null, false, false, false);
 			ordinazione.setNumTavolo(1);
-			ordinazione.setPizza(pizza = grafica.comboBoxOrdinazioni.getSelectedItem().toString());
+			if(grafica.comboBoxOrdinazioni.getSelectedIndex() == 0) {
+				System.out.println("Devi inserire un ordinazione valida");
+			}
+			else {
+				ordinazione.setPizza(pizza = grafica.comboBoxOrdinazioni.getSelectedItem().toString());
+			}
 			ordinazione.setPizzaConsegnata(true);
 			ordinazione.setSegnalazioneCameriere(true);
 			ordinazione.setSegnalazioneCucina(true);
 			
 			String gigio = ordinazione.toString();
-			
 			System.out.println(gigio);
 			
+			FileOutputStream fos;
+			ObjectOutputStream oos;
+			
+			try {
+				fos = new FileOutputStream("elenco.dat");
+				oos = new ObjectOutputStream(fos);
+				fos.close();
+			} catch (IOException e2 ) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+				
+			}
+		
 		}
 		
 	}
