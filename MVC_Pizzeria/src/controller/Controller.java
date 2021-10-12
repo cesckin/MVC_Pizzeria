15bb657de1e35dc151828313e0b6971e1c77a649
Francesco <Francesco@Win10Mac>
2021-10-12 12:45:18 +0200

    Controller update

--- MVC_Pizzeria/src/controller/Controller.java
+++ MVC_Pizzeria/src/controller/Controller.java
@@ -11,57 +11,55 @@ import model.Ordinazione;
 import view.Grafica;
 
 public class Controller implements ActionListener {
-	
+
 	private Grafica grafica;
-	
+
 	public Controller(Grafica grafica) {
-		
-		this.grafica=grafica;
+
+		this.grafica = grafica;
 		grafica.registraController(this);
-		
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+
 		if (e.getActionCommand().equalsIgnoreCase("Invia")) {
-			
-			String pizza;
-			
+
+			String pizza = "";
+
 			Ordinazione ordinazione = new Ordinazione(0, null, false, false, false);
 			ordinazione.setNumTavolo(1);
-			if(grafica.comboBoxOrdinazioni.getSelectedIndex() == 0) {
+			if (grafica.comboBoxOrdinazioni.getSelectedIndex() == 0) {
 				System.out.println("Devi inserire un ordinazione valida");
-			}
-			else {
+			} else {
 				ordinazione.setPizza(pizza = grafica.comboBoxOrdinazioni.getSelectedItem().toString());
 			}
 			ordinazione.setPizzaConsegnata(true);
 			ordinazione.setSegnalazioneCameriere(true);
 			ordinazione.setSegnalazioneCucina(true);
-			
+
 			String gigio = ordinazione.toString();
 			System.out.println(gigio);
-			
-			//Inserimento comande
+
+			// Inserimento comande
 			FileOutputStream fos;
 			ObjectOutputStream oos;
-			
+
 			try {
 				fos = new FileOutputStream("Comande.dat");
 				oos = new ObjectOutputStream(fos);
 				oos.writeObject(ordinazione);
 				oos.flush();
 				fos.close();
-			} catch (IOException e1 ) {
+			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-		
-			System.out.println("Cameriere ha inserito "+prodotto+" nell'ordinazione");
-			
-			
+
+			System.out.println("Cameriere ha inserito " + pizza + " nell'ordinazione");
+
 		}
-		
+
 	}
 
 }
