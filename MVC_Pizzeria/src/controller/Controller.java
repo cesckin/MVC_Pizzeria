d1fbff4b724283db4f5b3a14acea148360fd139e
Francesco <Francesco@Win10Mac>
2021-10-18 13:00:30 +0200

    On work

--- MVC_Pizzeria/src/controller/Controller.java
+++ MVC_Pizzeria/src/controller/Controller.java
@@ -2,9 +2,11 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import model.Ordinazione;
@@ -27,21 +29,22 @@ public class Controller implements ActionListener {
 
 		if (e.getActionCommand().equalsIgnoreCase("Ordina")) {
 			
-			boolean segnalazioneCucina = false;
+			boolean segnalazioneCucina = false; //per il cameriere
 			boolean pizzaConsegnata = false;
-			boolean segnalazioneCameriere = false;
+			boolean segnalazioneCameriere = false; //per la cucina
 			
 			Thread thread = new Thread();
 			
 			Ordinazione ordinazione = new Ordinazione("", "");
 
-			FileOutputStream fos;
-			ObjectOutputStream oos;
+			
 
 			ordinazione.setPizza(grafica.comboBoxOrdinazioni.getSelectedItem().toString());
 			ordinazione.setNumTavolo(grafica.comboBoxTavoli.getSelectedItem().toString());
 
 			try {
+				FileOutputStream fos = null;
+				ObjectOutputStream oos = null;
 				fos = new FileOutputStream("Ordini.lin");
 				oos = new ObjectOutputStream(fos);
 				oos.writeObject(ordinazione);
@@ -53,10 +56,31 @@ public class Controller implements ActionListener {
 			
 			try {
 				thread.wait(2000);
-			} catch (InterruptedException e1) {
+			} catch (InterruptedException e1) {}
+			
+			segnalazioneCucina = true;
+			if(segnalazione == true) {
+				grafica.lblCameriere.
 			}
 			
-			
+			FileInputStream fis = null;
+			ObjectInputStream ois = null;
+			try {
+				fis = new FileInputStream("Elenco.dat");
+				ois = new ObjectInputStream(fis);
+			} catch (IOException e1) {}
+			int i = 0;
+			boolean continua = true;
+			while (continua) {
+				try {
+					Ordinazione ordinazione1 = (Ordinazione) ois.readObject();
+					System.out.println(i);
+					System.out.println(ordinazione1);
+				} catch (ClassNotFoundException | IOException e1) {
+					continua = false;
+				}
+				i++;
+			}
 
 		}
 
