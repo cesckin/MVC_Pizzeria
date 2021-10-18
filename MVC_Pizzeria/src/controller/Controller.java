1974f4cc36362e74ed055776f0e99bb7838f9b10
Francesco <Francesco@Win10Mac>
2021-10-18 12:42:15 +0200

    All is coming

--- MVC_Pizzeria/src/controller/Controller.java
+++ MVC_Pizzeria/src/controller/Controller.java
@@ -9,7 +9,6 @@ import java.io.ObjectOutputStream;
 
 import model.Ordinazione;
 import view.Grafica;
-import utility.Object;
 
 public class Controller implements ActionListener {
 
@@ -29,40 +28,33 @@ public class Controller implements ActionListener {
 		if (e.getActionCommand().equalsIgnoreCase("Invia")) {
 
 			String pizza = "";
-
 			Ordinazione ordinazione = new Ordinazione("", "");
 
-			if ((grafica.comboBoxOrdinazioni.getSelectedIndex() == 0) || (grafica.comboBoxTavoli.getSelectedIndex() == 0)) {
-				System.out.println("Devi inserire un ordinazione valida");
-			} else {
+			boolean segnalazioneCucina;
+			boolean pizzaConsegnata;
+			boolean segnalazioneCameriere;
 
-				boolean segnalazioneCucina;
-				boolean pizzaConsegnata;
-				boolean segnalazioneCameriere;
+			FileOutputStream fos;
+			ObjectOutputStream oos;
 
-				FileOutputStream fos;
-				ObjectOutputStream oos;
+			ordinazione.setPizza(pizza = grafica.comboBoxOrdinazioni.getSelectedItem().toString());
+			ordinazione.setNumTavolo(grafica.comboBoxTavoli.getSelectedItem().toString());
 
-				ordinazione.setPizza(pizza = grafica.comboBoxOrdinazioni.getSelectedItem().toString());
-				ordinazione.setNumTavolo(grafica.comboBoxTavoli.getSelectedItem().toString());
-				
-				//INUTILE
-				System.out.println("Cameriere ha inserito " + pizza + " nell'ordinazione");
-				System.out.println(grafica.comboBoxTavoli.getSelectedItem().toString());
-				String gigio = ordinazione.toString();
-				System.out.println(gigio);
-				//INUTILE
-				
-				try {
-					fos = new FileOutputStream("Comande.dat");
-					oos = new ObjectOutputStream(fos);
-					oos.writeObject(ordinazione);
-					oos.flush();
-					fos.close();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
+			// INUTILE
+			System.out.println("Cameriere ha inserito " + pizza + " nell'ordinazione");
+			System.out.println(grafica.comboBoxTavoli.getSelectedItem().toString());
+			String gigio = ordinazione.toString();
+			System.out.println(gigio);
+			// INUTILE
 
+			try {
+				fos = new FileOutputStream("Comande.dat");
+				oos = new ObjectOutputStream(fos);
+				oos.writeObject(ordinazione);
+				oos.flush();
+				fos.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
 			}
 
 		}
--- MVC_Pizzeria/src/view/Grafica.java
+++ MVC_Pizzeria/src/view/Grafica.java
@@ -12,11 +12,13 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import java.awt.Color;
 
 public class Grafica {
 
 	private JFrame frame;
-	private JButton btnInvia;
+	private JButton btnOrdina;
 	public JComboBox comboBoxOrdinazioni;
 	public JComboBox comboBoxTavoli;
 
@@ -48,32 +50,51 @@ public class Grafica {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 663, 541);
+		frame.setBounds(100, 100, 800, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		btnInvia = new JButton("Invia");
-		btnInvia.addActionListener(new ActionListener() {
+		btnOrdina = new JButton("Ordina");
+		btnOrdina.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnInvia.setBounds(23, 11, 89, 23);
-		frame.getContentPane().add(btnInvia);
+		btnOrdina.setBounds(40, 112, 89, 23);
+		frame.getContentPane().add(btnOrdina);
 
 		comboBoxOrdinazioni = new JComboBox();
 		comboBoxOrdinazioni.setModel(new DefaultComboBoxModel(
-				new String[] { "MENU", "Margherita", "Capricciosa", "Diavola", "Quattro formaggi", "Ananas" }));
+				new String[] { "Margherita", "Capricciosa", "Diavola", "Quattro formaggi", "Ananas" }));
 		comboBoxOrdinazioni.setBounds(23, 44, 106, 22);
 		frame.getContentPane().add(comboBoxOrdinazioni);
-		
+
 		comboBoxTavoli = new JComboBox();
-		comboBoxTavoli.setModel(new DefaultComboBoxModel(new String[] { "NUMERO TAVOLO", "1", "2", "3", "4", "5","6","7","8","9","10" }));
+		comboBoxTavoli.setModel(new DefaultComboBoxModel(
+				new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" }));
 		comboBoxTavoli.setBounds(23, 79, 129, 22);
 		frame.getContentPane().add(comboBoxTavoli);
+		
+		JLabel lblCucina = new JLabel("New label");
+		lblCucina.setBounds(359, 28, 48, 14);
+		frame.getContentPane().add(lblCucina);
+		
+		JLabel lblCameriere = new JLabel("New label");
+		lblCameriere.setBounds(273, 200, 48, 14);
+		frame.getContentPane().add(lblCameriere);
+		
+		JPanel cucinaPronto = new JPanel();
+		cucinaPronto.setBackground(Color.GREEN);
+		cucinaPronto.setBounds(374, 79, 10, 10);
+		frame.getContentPane().add(cucinaPronto);
+		
+		JPanel cucinaPreparo = new JPanel();
+		cucinaPreparo.setBackground(Color.RED);
+		cucinaPreparo.setBounds(394, 79, 10, 10);
+		frame.getContentPane().add(cucinaPreparo);
 	}
 
 	public void registraController(Controller controller) {
-		btnInvia.addActionListener(controller);
+		btnOrdina.addActionListener(controller);
 	}
 
 	public void setVisible(boolean bho) {
